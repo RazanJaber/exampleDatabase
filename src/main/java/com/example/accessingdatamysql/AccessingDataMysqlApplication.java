1dada77cae519046b74719527d2df930e374d10a
RazanJaber <razan.jabir@gmail.com>
2021-04-28 13:50:10 +0200

    initalizer

--- src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -2,12 +2,25 @@ package com.example.accessingdatamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class AccessingDataMysqlApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("hello %s", name);
+		
+	}
+	
 
 }
